f1a02b5dd5c80b0b1f7d195779a065a75c1013be
MelvinFredrickJS <145590778+MelvinFredrickJS@users.noreply.github.com>
2025-01-02 19:38:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subarrays with sum K/subarrays-with-sum-k.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        // taking input using class Scanner
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            // taking total number of elements
+            int k = Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+            int res = new Solution().countSubarrays(arr, k);
+
+            System.out.print(res);
+            System.out.println();
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    public int countSubarrays(int arr[], int k) {
+    Map<Integer, Integer> prefixSums = new HashMap<>();
+        int res = 0;
+        int currSum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            // Add current element to sum so far.
+            currSum += arr[i];
+
+            // If currSum is equal to desired sum, then a new subarray is found.
+            if (currSum == k)
+                res++;
+
+            // Check if the difference exists in the prefixSums map.
+            if (prefixSums.containsKey(currSum - k))
+                res += prefixSums.get(currSum - k);
+
+            // Add currSum to the set of prefix sums.
+            prefixSums.put(currSum, prefixSums.getOrDefault(currSum, 0) + 1);
+        }
+
+        return res;    
+}
+}
