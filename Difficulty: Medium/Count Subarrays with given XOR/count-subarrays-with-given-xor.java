fefd35f1eec6f2d383786354bbc030c33d2b1839
MelvinFredrickJS <145590778+MelvinFredrickJS@users.noreply.github.com>
2025-01-03 19:03:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Subarrays with given XOR/count-subarrays-with-given-xor.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+
+        while (tc-- > 0) {
+            String s[] = br.readLine().split(" ");
+            int arr[] = new int[s.length];
+
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = Integer.parseInt(s[i]);
+            }
+            int k = Integer.parseInt(br.readLine());
+
+            Solution obj = new Solution();
+            System.out.println(obj.subarrayXor(arr, k));
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public long subarrayXor(int arr[], int k) {
+       int res = 0;
+
+        // Create map that stores number of prefix arrays
+        // corresponding to a XOR value
+        HashMap<Integer, Integer> mp = new HashMap<>();
+
+        int prefXOR = 0;
+
+        for (int val : arr) {
+          
+            // Find XOR of current prefix
+            prefXOR ^= val;
+
+            // If prefXOR ^ k exists in mp then there is a subarray
+            // ending at i with XOR equal to k
+            res += mp.getOrDefault(prefXOR ^ k, 0);
+
+            // If this prefix subarray has XOR equal to k
+            if (prefXOR == k)
+                res++;
+
+            // Add the XOR of this subarray to the map
+            mp.put(prefXOR, mp.getOrDefault(prefXOR, 0) + 1);
+        }
+
+        // Return total count of subarrays having XOR of
+        // elements as given value k
+        return res;
+    }
+}
