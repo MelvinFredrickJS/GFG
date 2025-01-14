2c2cd273254f0886a0aac9b3e14157b7c5ecea5b
MelvinFredrickJS <145590778+MelvinFredrickJS@users.noreply.github.com>
2025-01-14 10:49:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Equilibrium Point/equilibrium-point.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(
+                    token)); // Use Integer.parseInt to parse int values
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            Solution obj = new Solution();
+
+            // calling equilibriumPoint() function
+            System.out.println(obj.findEquilibrium(arr));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to find equilibrium point in the array.
+    public static int findEquilibrium(int arr[]) {
+        // code here
+                int prefSum = 0, total = 0;
+
+        // Calculate the array sum
+        for (int ele : arr) {
+            total += ele;
+        }
+
+        // Iterate pivot over all the elements of the array
+        for (int pivot = 0; pivot < arr.length; pivot++) {
+            int suffSum = total - prefSum - arr[pivot];
+            if (prefSum == suffSum) {
+                return pivot;
+            }
+            prefSum += arr[pivot];
+        }
+
+        // There is no equilibrium point
+        return -1;
+    }
+}
